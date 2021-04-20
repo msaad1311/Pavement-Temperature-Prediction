c4276ff2eef285c8e1745951dec262f0f89a9930
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-04-20 13:48:13 -0400

    added the python scripts

---
+++ Code/Source/utils.py
@@ -0,0 +1,125 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error as mse
+
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
+from tensorflow import keras
+from tensorflow.keras.layers import *
+import tensorflow.keras.backend as K
+
+print('Libraries Loaded')
+def read_file(path):
+    '''
+    Returns the dataframe which is read from the excel file present in the path specified. 
+    
+    Parameters:
+        path (str) : The path of the file
+    
+    Returns:
+        df (float) : The dataframe which is created after reading the file.
+    '''
+    df= pd.read_excel(path)
+    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+    print(df.shape)
+    print(df.head())
+    return df
+
+def create_dataset(X, y, time_steps, ts_range):
+    '''
+    Returns the prepared data based on the lag and look ahead
+    
+    Parameters:
+        X          (float): The independent variables of the data
+        y          (float): The dependent variables of the data
+        time_steps (int)  : The lag that is being used to lookback
+        ts_range   (int)  : The lookahead for the data
+    
+    Returns:
+        Xs (float) : The numpy array of the input variable
+        ys (float) : The numpy array of the output variable 
+    '''
+    Xs, ys = [], []
+    for i in range(len(X) - time_steps - ts_range):
+        v = X.iloc[i:(i + time_steps)].values
+        Xs.append(v)
+        ys.append(y.values[(i + time_steps):(i + time_steps + ts_range),0])
+    return np.array(Xs), np.array(ys)
+
+def splitter(df,output,lag,duration,ts):
+    '''
+    Returns the training and testing data
+    
+    Parameters:
+        df (float): The whole dataframe containing the independent and dependent variables
+        output(str): The output variable 
+        lag (int): The lag that needs to be applied for the data
+        duration (int): The duration that is being considered as output
+        ts (float): The percentage of training data
+    
+    Returns:
+        x_train (float): The training data of independent variable 
+        x_test (float): The testing data of independent variable
+        y_train (float): The training data of the depenedent variable 
+        y_test (float): The testing data of the dependent variable 
+    '''
+    assert (0. <= ts <= 1.)
+    train_size = int(len(df) * ts)
+    test_size = len(df) - train_size
+    train, test = df.iloc[0:train_size], df[train_size:]
+    print(train.shape, test.shape)
+    scaler,scaler_single = MinMaxScaler(feature_range=(0, 1)),MinMaxScaler(feature_range=(0, 1))
+
+    scaler.fit(train)
+    scaler_single.fit(train[output])
+
+    train_scaled = pd.DataFrame(scaler.transform(train), columns=[df.columns])
+    test_scaled = pd.DataFrame(scaler.transform(test), columns=[df.columns])
+
+    df_train = train_scaled.copy(deep=True)
+    df_test = test_scaled.copy(deep=True)
+
+    x_train,y_train = create_dataset(df_train,df_train[[output]],lag,duration)
+    x_test, y_test = create_dataset(df_test, df_test[[output]], lag, duration)
+
+    return x_train,x_test,y_train,y_test,scaler_single
+
+class attention(keras.layers.Layer):
+    '''
+    Attention layer for the neural networks.
+    
+    if return_sequences=True, it will give 3D vector and if false it will give 2D vector. It is same as LSTMs.
+
+    https://stackoverflow.com/questions/62948332/how-to-add-attention-layer-to-a-bi-lstm/62949137#62949137
+    the  following code is being inspired from the above link.
+    '''
+
+    def __init__(self, return_sequences=True, **kwargs):
+        self.return_sequences = return_sequences
+        super(attention, self).__init__()
+
+    def get_config(self):
+        cfg = super().get_config()
+        return cfg
+
+    def build(self, input_shape):
+        self.W = self.add_weight(name="att_weight", shape=(input_shape[-1], 1),
+                                 initializer="normal")
+        self.b = self.add_weight(name="att_bias", shape=(input_shape[1], 1),
+                                 initializer="zeros")
+
+        super(attention, self).build(input_shape)
+
+    def call(self, x):
+        e = K.tanh(K.dot(x, self.W) + self.b)
+        a = K.softmax(e, axis=1)
+        output = x * a
+
+        if self.return_sequences:
+            return output
+
+        return K.sum(output, axis=1)
