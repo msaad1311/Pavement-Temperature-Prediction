8d7480e2beb0562610bf833fd69410c557f2ec64
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-04-20 15:03:40 -0400

    Update main.py

--- Code/Source/main.py
+++ Code/Source/main.py
@@ -1,1 +1,19 @@
-import
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import xlwt 
+from xlwt import Workbook 
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error as mse
+
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
+from tensorflow import keras
+from tensorflow.keras.layers import *
+import tensorflow.keras.backend as K
+
+print('Libraries Loaded')
