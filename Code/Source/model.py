5e24a1e16518bc5eb689ffc5aeedc200be86defa
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-04-21 16:44:44 -0400

    cleaned the code and ran the cnnlstm test

--- Code/Source/model.py
+++ Code/Source/model.py
@@ -1,14 +1,12 @@
 import os
 import pandas as pd
 import matplotlib.pylab as plt
+
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.metrics import mean_squared_error as mse
-import tensorflow as tf
-# physical_devices = tf.config.list_physical_devices('GPU')
-# tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
+
 from tensorflow import keras
 import tensorflow.keras.backend as K
-from tensorflow.python.keras.layers.normalization import BatchNormalization
 
 
 class attention(keras.layers.Layer):
@@ -47,46 +45,13 @@ class attention(keras.layers.Layer):
 
         return K.sum(output, axis=1)
 
-
-def model_fit(dest, directory, model, xtrain, ytrain, xtest, ytest, scaler, checkpoint, weights, sheet):
-    old_path = os.getcwd()
-    try:
-        os.chdir(os.path.join(dest, directory))
-        print('Directory present')
-    except FileNotFoundError:
-        print('Creating a new directory......')
-        os.chdir(os.path.join(dest))
-        os.mkdir(directory)
-        os.chdir(os.path.join(dest, directory))
-        print('New Directory Created')
-
-    history = model.fit(xtrain, ytrain, validation_split=0.1,
-                        batch_size=32, epochs=200, callbacks=[checkpoint])
-
-    plt.plot(history.history['loss'], 'r', label='Training Loss')
-    plt.plot(history.history['val_loss'], 'b', label='Validation Loss')
-    plt.legend()
-    plt.show()
-
-    model.load_weights(weights)
-    preds = model.predict(xtest)
-
-    ytest_unscaled = scaler.inverse_transform(ytest)
-    preds_unscaled = scaler.inverse_transform(preds)
-
-    for i in range(ytest.shape[1]):
-        sheet.write(0, 0, 'MSE')
-        sheet.write(0, 1, 'Hours Ahead')
-        sheet.write(i + 1, 0, mse(ytest_unscaled[:, i], preds_unscaled[:, i]))
-        sheet.write(i + 1, 1, i+1)
-    os.chdir(old_path)
-
-
 class buildLSTM(BaseEstimator, TransformerMixin):
-    def __init__(self, X, atten):
-        self.X = X
+    def __init__(self, xtrain,ytrain,scaler, atten):
+        self.xtrain = xtrain
+        self.ytrain = ytrain
+        self.scaler = scaler
         self.atten = atten
-        self.model = self.buildModel(self.X,self.atten)
+        self.model = self.buildModel(self.xtrain,self.atten)
         self.model.compile(loss='mse', optimizer=keras.optimizers.Adam(
             learning_rate=3e-4), metrics=['mae'])
 
@@ -121,44 +86,49 @@ class buildLSTM(BaseEstimator, TransformerMixin):
 
         return model
 
-    def fit(self, X, y):
+    def fit(self):
         checkpoint = self.checkpointer(self.atten)
-        self.history = self.model.fit(X, y, validation_split=0.1,
-                                      batch_size=32, epochs=10, callbacks=[checkpoint])
+        self.history = self.model.fit(self.xtrain, self.ytrain, validation_split=0.1,
+                                      batch_size=32, epochs=2, callbacks=[checkpoint])
         plt.plot(self.history.history['loss'], 'r', label='Training Loss')
         plt.plot(self.history.history['val_loss'],
                  'b', label='Validation Loss')
         plt.legend()
         plt.show()
 
-        return self.model
+        return self
 
-    def predict(self,scaler,xtest,ytest):
+    def predictions(self,xtest,ytest):
         if self.atten:
             filepath = '../Weights/LSTM/attention_lstm.hdf5'
         else:
             filepath = '../Weights/LSTM/simple_lstm.hdf5'
-        print(self.model)
         self.model.load_weights(filepath)
         preds = self.model.predict(xtest)
 
-        ytest_unscaled = scaler.inverse_transform(ytest)
-        preds_unscaled = scaler.inverse_transform(preds)
+        ytest_unscaled = self.scaler.inverse_transform(ytest)
+        preds_unscaled = self.scaler.inverse_transform(preds)
         
         results = []
         for i in range(ytest.shape[1]):
             results.append(mse(ytest_unscaled[:,i],preds_unscaled[:,i]))
         results = pd.DataFrame(results).reset_index()
-        path = '../Results/LSTM/MSE_{self.atten}.xlsx'
-        results.to_excel(path)
+        path = f'../Results/LSTM/MSE_{self.atten}.xlsx'
+        try:
+            results.to_excel(path)
+        except Exception as e:
+            print(e)
+            os.mkdir(os.path.split(path)[0])
+            results.to_excel(path)
         return self
     
-    
 class buildCNNLSTM(BaseEstimator, TransformerMixin):
-    def __init__(self, X, atten):
-        self.X = X
+    def __init__(self, xtrain,ytrain,scaler,atten):
+        self.xtrain = xtrain
+        self.ytrain = ytrain
+        self.scaler = scaler
         self.atten = atten
-        self.model = self.buildModel(self.X,self.atten)
+        self.model = self.buildModel(self.xtrain,self.atten)
         self.model.compile(loss='mse', optimizer=keras.optimizers.Adam(
             learning_rate=3e-4), metrics=['mae'])
 
@@ -190,10 +160,10 @@ class buildCNNLSTM(BaseEstimator, TransformerMixin):
 
         return model
 
-    def fit(self, X, y):
+    def fit(self):
         checkpoint = self.checkpointer(self.atten)
-        self.history = self.model.fit(X, y, validation_split=0.1,
-                                      batch_size=32, epochs=10, callbacks=[checkpoint])
+        self.history = self.model.fit(self.xtrain, self.ytrain, validation_split=0.1,
+                                      batch_size=32, epochs=2, callbacks=[checkpoint])
         plt.plot(self.history.history['loss'], 'r', label='Training Loss')
         plt.plot(self.history.history['val_loss'],
                  'b', label='Validation Loss')
@@ -202,7 +172,7 @@ class buildCNNLSTM(BaseEstimator, TransformerMixin):
 
         return self.model
 
-    def predict(self,scaler,xtest,ytest):
+    def predictions(self,xtest,ytest):
         if self.atten:
             filepath = '../Weights/CNNLSTM/attention_cnnlstm.hdf5'
         else:
@@ -211,75 +181,18 @@ class buildCNNLSTM(BaseEstimator, TransformerMixin):
         self.model.load_weights(filepath)
         preds = self.model.predict(xtest)
 
-        ytest_unscaled = scaler.inverse_transform(ytest)
-        preds_unscaled = scaler.inverse_transform(preds)
+        ytest_unscaled = self.scaler.inverse_transform(ytest)
+        preds_unscaled = self.scaler.inverse_transform(preds)
         
         results = []
         for i in range(ytest.shape[1]):
             results.append(mse(ytest_unscaled[:,i],preds_unscaled[:,i]))
         results = pd.DataFrame(results).reset_index()
-        path = '../Results/CNNLSTM/MSE_{self.atten}.xlsx'
-        results.to_excel(path)
+        path = f'../Results/CNNLSTM/MSE_{self.atten}.xlsx'
+        try:
+            results.to_excel(path)
+        except Exception as e:
+            print(e)
+            os.mkdir(os.path.split(path)[0])
+            results.to_excel(path)
         return self
-
-
-
-# def build_lstm(x_train, atten=False, dropout=False, regularizer=None):
-#     K.clear_session()
-#     model = keras.Sequential()
-#     model.add(keras.layers.BatchNormalization())
-#     model.add(keras.layers.LSTM(64, return_sequences=True,
-#               kernel_regularizer=regularizer, input_shape=(x_train.shape[1], x_train.shape[2])))
-#     model.add(keras.layers.BatchNormalization())
-#     model.add(keras.layers.LSTM(
-#         64, kernel_regularizer=regularizer, return_sequences=True))
-#     if dropout:
-#         model.add(keras.layers.Dropout(0.3))
-
-#     model.add(keras.layers.BatchNormalization())
-#     model.add(keras.layers.LSTM(
-#         64, kernel_regularizer=regularizer, return_sequences=True))
-#     model.add(keras.layers.BatchNormalization())
-#     model.add(keras.layers.LSTM(
-#         64, kernel_regularizer=regularizer, return_sequences=True))
-#     if atten:
-#         model.add(attention(return_sequences=True))
-#     model.add(keras.layers.Flatten())
-#     model.add(keras.layers.Dense(512, activation='relu'))
-#     model.add(keras.layers.Dense(128, activation='relu'))
-#     model.add(keras.layers.Dense(64, activation='relu'))
-#     if dropout:
-#         model.add(keras.layers.Dropout(0.3))
-
-#     model.add(keras.layers.Dense(32))
-#     model.add(keras.layers.Dense(6))
-
-#     model.compile(loss='mse', optimizer=keras.optimizers.Adam(
-#         learning_rate=0.001), metrics=['mae'])
-
-#     return model
-
-
-# def build_cnnlstm(x_train, atten=False, regularizer=None):
-#     model = keras.Sequential()
-#     model.add(keras.layers.add(BatchNormalization()))
-#     model.add(keras.layers.Conv1D(64, kernel_size=3, kernel_regularizer=regularizer,
-#               input_shape=(x_train.shape[1], x_train.shape[2])))
-#     model.add(keras.layers.add(BatchNormalization()))
-#     model.add(keras.layers.Conv1D(64, kernel_size=3))
-#     model.add(keras.layers.add(BatchNormalization()))
-#     model.add(keras.layers.LSTM(64, return_sequences=True))
-#     model.add(keras.layers.add(BatchNormalization()))
-#     model.add(keras.layers.LSTM(64, return_sequences=True))
-#     if atten:
-#         model.add(attention(return_sequences=True))
-#     model.add(keras.layers.Flatten())
-#     model.add(keras.layers.Dense(512, activation='relu'))
-#     model.add(keras.layers.Dense(128, activation='relu'))
-#     model.add(keras.layers.Dense(64, activation='relu'))
-#     model.add(keras.layers.Dense(32))
-#     model.add(keras.layers.Dense(6))
-
-#     model.compile(loss='mse', optimizer=keras.optimizers.Adam(
-#         learning_rate=0.0001), metrics=['mae'])
-#     return model
--- Code/Source/testing.py
+++ Code/Source/testing.py
@@ -1,31 +1,10 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-import xlwt 
-from xlwt import Workbook 
 import utils as u
 import model as m
-
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error as mse
-
-import tensorflow as tf
-# physical_devices = tf.config.list_physical_devices('GPU')
-# tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
-from tensorflow import keras
-from tensorflow.keras.layers import *
-import tensorflow.keras.backend as K
 
 print('Libraries Loaded')
 
 ## Specifying the source path
 src = r'D:\Pavement-Temperature-Prediction\Data\Pave_data_cleaned.xlsx'
-# filename = r'Pave_data_cleaned.xlsx'
-
-## Specifying the destination path
-dest = r'D:\Pavement-Temperature-Prediction\Results\Six Hours Lag'
 
 ## Reading the file
 df = u.read_file(src)
@@ -35,6 +14,7 @@ x_train,x_test,y_train,y_test,scaler = u.splitter(df[['Temp','Pavement']],['Pave
 print(f'The shape of x_train is {x_train.shape} and x_test is {x_test.shape}')
 print(f'The shape of y_train is {y_train.shape} and y_test is {y_test.shape}')
 
-model = m.buildLSTM(x_train,True)
-model = model.fit(x_train,y_train)
-model.predict(scaler,x_test,y_test)
+
+model1 = m.buildCNNLSTM(x_train,y_train,scaler,True)
+model1.fit()
+model1.predictions(x_test,y_test)
